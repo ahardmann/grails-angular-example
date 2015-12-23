d75617c22882480292a462679229fc7601b8c300
Arthur Hage <arthur.hage@C305-ads-036.fieb.org.br>
2015-12-23 15:12:14 -0300

    create a json resgister marshaller

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
+import todo.example.Tasks
+
 class BootStrap {
 
     def init = { servletContext ->
     }
+	
     def destroy = {
     }
+	
+	def registerMarshallers() {
+		JSON.registerObjectMarshaller(Tasks){
+			def returnMap = [:]
+			returnMap.name = it.name
+			returnMap.description = it.description
+			returnMap.date =  it.date
+			
+			return returnMap
+		}
+		
+	}
 }
