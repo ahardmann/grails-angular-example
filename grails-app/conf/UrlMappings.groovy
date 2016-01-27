5af4ca3bd938a2504dfe33628152a51cb97d3c7c
Arthur Hage <arthur.hage@C305-ads-036.fieb.org.br>
2016-01-27 16:20:26 -0300

    grails url mappings

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,18 @@
 class UrlMappings {
 
 	static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
+		"/$controller/$action?/$id?(.$format)?"{
+
+		}
+
+		"/api/$controller/"(action: 'index', method: 'GET')
+		"/api/$controller/"(action: 'save', method: 'POST')
+		"/api/$controller/$id?"(action: 'show', method: 'GET')
+		"/api/$controller/$id?"(action: 'update', method: 'PUT')
+		"/api/$controller/$id?"(action: 'delete', method: 'DELETE')
+
+		"500"(view:'/error')
 	}
+
 }
+	
