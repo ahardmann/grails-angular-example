f78771584ca346071738470dd56139ed884b4cfe
Arthur Hage <arthur.hage@C305-ads-036.fieb.org.br>
2016-01-28 14:03:47 -0300

    update grails controller and service to restfull

--- grails-app/controllers/todo/example/TasksController.groovy
+++ grails-app/controllers/todo/example/TasksController.groovy
@@ -1,25 +1,11 @@
 package todo.example
 
-import java.util.List;
-import java.util.Map;
-
 import grails.rest.RestfulController;
 
-class TasksController extends RestfulController<Tasks>{
-	def tasksService
-	
+class TasksController extends RestfulController{
+	static responseFormats = ['json', 'xml']
+
 	TasksController (){
 		super(Tasks)
 	}
-	
-	@Override
-	protected List<Tasks> listAllResources(Map params) {
-		return tasksService.findAllByFilter(params)
-	}
-
-	@Override
-	protected Integer countResources() {
-		return tasksService.countByFilter(params)
-	}
-
 }
